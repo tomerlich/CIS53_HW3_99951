72bdcc4d5f670a25a01b5e18218d3e3d7997e816
tomerlich <tomer.erlich2@gmail.com>
2017-04-29 12:27:42 -0700

    The code should all be done now

--- app/src/main/java/edu/deanza/cis53_hw3_99951/MainActivity.java
+++ app/src/main/java/edu/deanza/cis53_hw3_99951/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String APP_TAG = "CIS53_HW3_99951";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.d(APP_TAG, "onOptionsItemSelected Settings");
+            return true;
+        } else if (id == R.id.action_refresh){
+            Log.d(APP_TAG, "onOptionsItemSelected Refresh");
             return true;
         }
 
--- app/src/main/java/edu/deanza/cis53_hw3_99951/MainActivityFragment.java
+++ app/src/main/java/edu/deanza/cis53_hw3_99951/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,7 +22,25 @@ public class MainActivityFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+            Bundle savedInstanceState) {
+        String[] data = {
+                "dummy data1",
+                "dummy data2",
+                "dummy data3"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        ArrayAdapter<String> forecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_textview,
+                        weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+
+        return rootView;
     }
 }
